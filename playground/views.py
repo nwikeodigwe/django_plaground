d0ea5137485b277847c5fd2ed30cd97f68872b6d
Nwike <nwikeodigwe@outlook.com>
2024-07-29 04:00:39 -0700

    HTML Page

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
+
 
 # Create your views here.
 # A view function is a function that takes a request and returns a response
 
 def say_hello(request):
-    return HttpResponse('Hello World!')
+    return render(request, 'hello.html', {"name": "Nwike"})
